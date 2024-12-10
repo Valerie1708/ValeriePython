afea4faa27f73577f03d2a7fbab2760480522e96
Admin <valerieivanova89@gmail.com>
2024-12-11 00:48:57 +0300

    PZ3 Код

--- PZ3/PZ 3.2.py
+++ PZ3/PZ 3.2.py
@@ -1,25 +1,22 @@
-def get_days_in_month(month):
-    """
-    :param month: Номер месяца (1-12)
-    :type month: int
-    :return: Количество дней в месяце
-    :rtype: int
-    :raises ValueError: Если месяц вне диапазона 1-12
-    """
-    if month in (1, 3, 5, 7, 8, 10, 12):
-        return 31
-    elif month in (4, 6, 9, 11):
-        return 30
-    elif month == 2:
-        return 28
-    else:
-        raise ValueError(f'Недопустимый номер месяца: {month}')
-def main():
-    try:
-        month = int(input('Введите номер месяца (1-12): '))
-        days = get_days_in_month(month)
-        print(f'В месяце {month} {days} дней.')
-    except ValueError as e:
-        print(f'Ошибка: {e}')
-if __name__ == '__main__':
-    main()
+month_number = int(input("Введите номер месяца (1-12): "))
+
+days_in_month = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+try:
+    days = days_in_month[month_number]
+    print(f"В месяце под номером {month_number} {days} дней.")
+except KeyError:
+    print("Ошибка: Номер месяца должен быть в диапазоне от 1 до 12.")

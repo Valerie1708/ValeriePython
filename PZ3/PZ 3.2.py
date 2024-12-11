7ceaa64808e59e0196bdedca598ca60a0dbfb0df
Admin <valerieivanova89@gmail.com>
2024-12-11 15:53:24 +0300

    PZ3 Код

--- PZ3/PZ 3.2.py
+++ PZ3/PZ 3.2.py
@@ -1,22 +1,10 @@
 month_number = int(input("Введите номер месяца (1-12): "))
 
-days_in_month = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31
-}
+if month_number in (1, 3, 5, 7, 8, 10, 12):
+    days = 31
+elif month_number == 2:
+    days = 28
+else:
+    days = 30
 
-try:
-    days = days_in_month[month_number]
-    print(f"В месяце под номером {month_number} {days} дней.")
-except KeyError:
-    print("Ошибка: Номер месяца должен быть в диапазоне от 1 до 12.")
+print(f"В {month_number}-м месяце {days} дней.")

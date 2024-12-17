006ae67d387c1cd62d4b662e0f031985a77eb032
Admin <valerieivanova89@gmail.com>
2024-12-17 22:54:12 +0300

    PZ_3 Код

--- PZ3/PZ 3.2.py
+++ PZ3/PZ 3.2.py
@@ -1,10 +1,16 @@
-month_number = int(input("Введите номер месяца (1-12): "))
+# Дан номер месяца — целое число в диапазоне 1-12 (1 — январь, 2 — февраль и т. д.).
+# Определить количество дней в этом месяце для невисокосного года.
+try:
+    month_number = int(input("Введите номер месяца (1-12): "))
+    if not 1 <= month_number <= 12:
+        raise ValueError("Номер месяца должен быть в диапазоне от 1 до 12")
+    if month_number in (1, 3, 5, 7, 8, 10, 12):
+        days = 31
+    elif month_number == 2:
+        days = 28
+    else:
+        days = 30
+    print(f"В {month_number}-м месяце {days} дней.")
+except ValueError as e:
+    print("Ошибка:", e)
 
-if month_number in (1, 3, 5, 7, 8, 10, 12):
-    days = 31
-elif month_number == 2:
-    days = 28
-else:
-    days = 30
-
-print(f"В {month_number}-м месяце {days} дней.")
